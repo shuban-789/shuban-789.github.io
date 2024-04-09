6322620e88dc698e007a52eed92533c6692d31bb
shuban-789 <cybergeek11929@gmail.com>
2024-04-09 00:59:44 -0700

    getting deployment to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,9 @@ import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import { astroImageTools } from 'astro-imagetools';
 
-// https://astro.build/config
 export default defineConfig({
-  // base: '.', // Set a path prefix.
-  site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
+  base: '/src/pages/index.astro', // Set a path prefix.
+  site: 'https://shuban-789.github.io/', // Use to generate your sitemap and canonical URLs in your final build.
   trailingSlash: 'always', // Use to always append '/' at end of url
   markdown: {
     shikiConfig: {
