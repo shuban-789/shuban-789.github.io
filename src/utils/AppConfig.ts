48627691bdc55cef672a93d75d712bccd2e7163b
shuban-789 <cybergeek11929@gmail.com>
2024-08-24 12:54:20 -0700

    personalization commit

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'Shuban Pal | Blog',
+  title: "shuban-789 | Blog",
+  description: "Shuban's blog",
+  author: 'Shuban Pal',
   locale_region: 'en-us',
   locale: 'en',
 };
