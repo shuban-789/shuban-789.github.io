6eee70145a91c5a945bd573cb1dd2e3f05b7d5ee
shuban-789 <cybergeek11929@gmail.com>
2024-08-25 14:06:47 -0700

    footer edit

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,12 +1,10 @@
 import { Section } from 'astro-boilerplate-components';
 
-import { AppConfig } from '@/utils/AppConfig';
-
 const Footer = () => (
   <Section>
     <footer>
       {/* Custom footer content */}
-      <p>&copy; {new Date().getFullYear()} {AppConfig.site_name}. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Shuban Pal. All rights reserved.</p>
     </footer>
   </Section>
 );
