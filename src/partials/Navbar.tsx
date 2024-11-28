e85e820f4a12962446a0fa2d7b2cace16b5cb850
shuban-789 <cybergeek11929@gmail.com>
2024-11-28 14:34:36 -0800

    test

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -23,16 +23,16 @@ const Navbar = () => (
       </a>
 
       <NavMenu>
-        <span className="text-cyan-400 text-lg">
+        <span className="text-cyan-400 text-xl">
           <NavMenuItem href="/posts/">/blogs</NavMenuItem>
         </span>
-        <span className="text-cyan-400 text-lg">
+        <span className="text-cyan-400 text-xl">
           <NavMenuItem href="https://devpost.com/shuban-789">/devpost</NavMenuItem>
         </span>
-        <span className="text-cyan-400 text-lg">
+        <span className="text-cyan-400 text-xl">
           <NavMenuItem href="https://ctftime.org/user/189279">/ctftime</NavMenuItem>
         </span>
-        <span className="text-cyan-400 text-lg">
+        <span className="text-cyan-400 text-xl">
           <NavMenuItem href="https://leetcode.com/u/shuban-789/">/leetcode</NavMenuItem>
         </span>
       </NavMenu>
